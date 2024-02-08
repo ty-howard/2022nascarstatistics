74a17d1f80002da15ae6db6db989d9659df717f2
Ty <trhoward@email.sc.edu>
2024-02-08 09:14:48 -0500

    added feb 8 example

---
+++ examples/feb 8/script.js
@@ -0,0 +1,22 @@
+const updateThermometer = () => {
+    const goal = 10000;
+    const funds = document.getElementById("txt-funds").value;
+    const errorSpan = document.getElementById("funds-err");
+    errorSpan.innerHTML = "";
+    const therm = document.getElementById("thermometer");
+    document.querySelector(":root").style.setProperty("--funds", "0%");
+
+    if(isNaN(funds)){
+        errorSpan.innerHTML = "Not a valid number";
+        return;
+    }
+    else if(funds < 0){
+        errorSpan.innerHTML = "Negative number";
+        return;
+    }
+
+    const percent = funds/goal * 100;
+    document.querySelector(":root").style.setProperty("--funds", percent + "%");
+};
+
+document.getElementById("btn-cont").onclick = updateThermometer;
